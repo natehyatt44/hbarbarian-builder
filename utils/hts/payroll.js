9519ed7078a27e4695f0f26a2021ea12740b5604
natehyatt44 <nate.hyatt@gmail.com>
2023-04-19 10:10:51 -0600

    Updated payroll for royalties

--- utils/hts/payroll.js
+++ utils/hts/payroll.js
@@ -22,29 +22,41 @@ const {
   const client = Client.forTestnet().setOperator(treasuryId, treasuryKey);
   
   async function main() {
-    const prevWeekBalance = 20.00   // Replace with previous weeks Balance
+    const cfpRoyalty = 0.07
+
+    // Replace with previous weeks Balance
+    const prevWeekBalance = 0  
+    const prevWeekCFPTradingVolume = 0
+
     const currWeekBalance = await getHbarBalance(treasuryId);
-    const weeklyEarnings = Number((currWeekBalance - prevWeekBalance).toFixed(2));
-  
-    const founderShare = Number((weeklyEarnings * 0.20).toFixed(2)); // Founders = 20% Share
-    const lawyerShare = Number((weeklyEarnings * 0.03).toFixed(2)); // Lawyer = 3% Share
-    const companyShare = Number((weeklyEarnings * .37).toFixed(2)); // Company Share = 17% (Right now it's 37 due to Supernal/Poncho Sitch) 
+    const currWeekEarnings = Number((currWeekBalance - prevWeekBalance).toFixed(2)); // Includes both 
+
+    const currWeekCFPTradingVolume = 100
+    const currWeekCFPRoyaltyEarnings = (currWeekCFPTradingVolume - prevWeekCFPTradingVolume) * cfpRoyalty
+    
+    const founderShare = Number((currWeekEarnings * 0.20).toFixed(2)); // Founders = 20% Share
+    const companyShare = Number((currWeekEarnings * .17).toFixed(2)); // Company Share = 17% 
+    const employeeShare = Number(((currWeekEarnings - currWeekCFPRoyaltyEarnings) * .10).toFixed(2)); // Employee = 10% Share with no royalties
+    const lawyerShare = Number((currWeekEarnings * 0.03).toFixed(2)); // Lawyer = 3% Share
     const remainingBalance = Number((prevWeekBalance + companyShare).toFixed(2));
     
     console.log(`Amount of Company Wallet HBAR at end of last payroll: ${prevWeekBalance}`);
     console.log(`Amount of Company Wallet HBAR currently: ${currWeekBalance}`);
-    console.log(`Amount of HBAR made within this week: ${weeklyEarnings}`);
+    console.log(`Amount of HBAR made within this week: ${currWeekEarnings}`);
+    console.log(`Amount of HBAR made from Royalties within this week: ${currWeekCFPRoyaltyEarnings}`);
     console.log(`Amount HBAR to Company: ${companyShare}`);
     console.log(`Amount HBAR to Jman (Founder): ${founderShare}`);
     console.log(`Amount HBAR to Nbreezay (Founder): ${founderShare}`);
     console.log(`Amount HBAR to Alixon (Founder): ${founderShare}`);
+    console.log(`Amount HBAR to Supernal (Employee): ${employeeShare}`);
+    console.log(`Amount HBAR to Poncho (Employee): ${employeeShare}`);
     console.log(`Amount HBAR to Brett (Lawyer): ${lawyerShare}`);
-    console.log(`Amount that will remain in the company wallet after distribution: ${remainingBalance}`);
     
     // Any Left over small amount to balance the transaction. This will go back to the company
-    const leftOverAmt = Number((weeklyEarnings - companyShare - founderShare - founderShare - founderShare - lawyerShare).toFixed(2))
+    const leftOverAmt = Number((currWeekEarnings - companyShare - founderShare - founderShare - founderShare - employeeShare - employeeShare - lawyerShare).toFixed(2))
 
     console.log(`Amount of small leftover required to balance the transaction (Goes to Company Wallet): ${leftOverAmt}`);
+    console.log(`Amount that will remain in the company wallet after distribution: ${remainingBalance + leftOverAmt}`);
 
     // Prompt for confirmation before executing the transaction
     const confirm = readlineSync.question(`Do you want to proceed with the transaction? (y/n): `);
@@ -54,12 +66,14 @@ const {
     }
 
     const sendHbar = await new TransferTransaction()
-      .addHbarTransfer(treasuryId, Hbar.from(-weeklyEarnings))
+      .addHbarTransfer(treasuryId, Hbar.from(-currWeekEarnings))
       .addHbarTransfer(treasuryId, Hbar.from(companyShare))
       .addHbarTransfer(treasuryId, Hbar.from(leftOverAmt))
       .addHbarTransfer(jmanId, Hbar.from(founderShare))
       .addHbarTransfer(nbreezayId, Hbar.from(founderShare))
       .addHbarTransfer(alixonId, Hbar.from(founderShare))
+      .addHbarTransfer(supernalId, Hbar.from(employeeShare))
+      .addHbarTransfer(supernalId, Hbar.from(employeeShare))
       .addHbarTransfer(brettId, Hbar.from(lawyerShare))
       .execute(client);
   
